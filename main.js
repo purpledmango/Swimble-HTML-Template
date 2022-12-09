3bf88ad33c1fcb9ffa7106313c046c0cbc3e7f36
purpledmango <nitishkchauhan98@gmail.com>
2022-12-09 22:40:10 +0530

    first commit

--- main.js
+++ main.js
@@ -1,45 +1,3 @@
-// let menuOpenBtn = document.getElementById("mobile-menu-open");
-// let menuCloseBtn = document.getElementById("mobile-menu-close");
-// let menu = document.getElementById("menu");
-
-// let isMenuOpen = false;
-
-
-// function burgerMenuClicked() {
-// 	if (isMenuOpen == false){
-// 		menuOpenBtn.classList.toggle("hide");
-// 		menuCloseBtn.classList.toggle("show");
-// 		menu.classList.toggle("menu");
-// 		isMenuOpen = true;
-// 	}
-
-// 	else if (isMenuOpen == true){
-// 		menuOpenBtn.classList.toggle("hide");
-// 		menuCloseBtn.classList.toggle("show");
-// 		menu.classList.toggle("menu");
-// 		isMenuOpen = false;
-	
-// 	}	
-	
-// }
-
-
-
-// function initialize() {
-// 	console.log("Initialized")
-// }
-
-
-
-
-
-
-// $(window).load(function(){
-//    		// PAGE IS FULLY LOADED  
-//    		// FADE OUT YOUR OVERLAYING DIV
-//    		
-// 	});
-
 $("#mobile-menu-open").click(function(){
 	$("#mobile-menu-open").hide();
 	$("#menu").slideDown();
@@ -52,8 +10,6 @@ $("#mobile-menu-close").click(function(){
 	$("#menu").slideUp();
 });
 
-
-
 $(document).ready(function() {
-	$('#load').fadeOut();
+	$('#load').hide();
 });
